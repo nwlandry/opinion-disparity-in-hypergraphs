71e62cf12ff748ea1e38523d8972ec9fa2d367c4
nwlandry <nicholas.landry.91@gmail.com>
2022-10-05 13:32:07 -0400

    updated code

--- imbalanced_polarization_phase_space.jl
+++ imbalanced_polarization_phase_space.jl
@@ -15,8 +15,10 @@ end
 unzip(a) = map(x->getfield.(a, x), fieldnames(eltype(a)))
 
 function get_polarization(k, q, rho, epsilon2, epsilon3, beta2, beta3)
-    psi_max = 0
-    nu_max = Inf
+    psi1_max = 0
+    psi2_max = 0
+    nu1_max = Inf
+    nu2_max = Inf
 
     r2 = 1/(rho^2 + (1 - rho)^2) - 1
     r3 = 1/(rho^3 + (1 - rho)^3) - 1
@@ -32,7 +34,7 @@ function get_polarization(k, q, rho, epsilon2, epsilon3, beta2, beta3)
     function jacobian((x1, x2))
         J = zeros(2, 2)
 
-        J[1, 1] = -gamma + beta2*k*rho*(1 - x1)*(1 + r2*epsilon2) -
+        J[1, 1] = -1 + beta2*k*rho*(1 - x1)*(1 + r2*epsilon2) -
         beta2*k*((1 - rho)*(1 - epsilon2)*x2 + rho*(1 + r2*epsilon2)*x1) +
         beta3*q*(1 - x1)*(2*(1 - rho)^2*(1 - epsilon3)*x2 + 2*rho^2*(1 + r3*epsilon3)*x1) -
         beta3*q*((1 - rho)^2*(1 - epsilon3)*(2*x1*x2 + x2^2) + rho^2*(1 + r3*epsilon3)*x1^2)
@@ -41,7 +43,7 @@ function get_polarization(k, q, rho, epsilon2, epsilon3, beta2, beta3)
 
         J[2, 1] = rho*(1 - x2)*(beta2*k*(1 - epsilon2) + 2*beta3*rho*q*(1 - epsilon3)*(x1 + x2))
 
-        J[2, 2] = -gamma + beta2*k*(1 - rho)*(1 - x2)*(1 + r2*epsilon2) -
+        J[2, 2] = -1 + beta2*k*(1 - rho)*(1 - x2)*(1 + r2*epsilon2) -
         beta2*k*(rho*(1 - epsilon2)*x1 + (1 - rho)*(1 + r2*epsilon2)*x2) +
         beta3*q*(1 - x2)*(2*rho^2*(1 - epsilon3)*x1 + 2*(1 - rho)^2*(1 + r3*epsilon3)*x2) -
         beta3*q*(rho^2*(1 - epsilon3)*(2*x2*x1 + x1^2) + (1 - rho)^2*(1 + r3*epsilon3)*x2^2)
@@ -53,96 +55,87 @@ function get_polarization(k, q, rho, epsilon2, epsilon3, beta2, beta3)
         return maximum(real(eigvals(J)))
     end
     
-    result = roots(f, IntervalBox(0.0..0.5, 0.5..1.0))
+    result = roots(f, IntervalBox(0..1, 0..1))
     for rt in result
         x = mid(rt.interval)
-        psi = abs(x[1] - x[2])
+        nu = spectral_abscissa(jacobian(x))
 
-
-        if psi > 0.01 # filter out symmetric roots to save time
-            nu = spectral_abscissa(jacobian(x))
-            
-            if (psi > psi_max && nu < 0)
-                psi_max = psi
-                nu_max = nu
-            end
+        if (x[1] - x[2] > max(psi1_max, 0.01) && nu < 0)
+            psi1_max = x[1] - x[2]
+            nu1_max = nu
+        elseif (x[2] - x[1] > max(psi2_max, 0.01) && nu < 0)
+            psi2_max = x[2] - x[1]
+            nu2_max = nu
         end
     end
 
-    return psi_max, nu_max
+    return psi1_max, nu1_max, psi2_max, nu2_max
 end
 
 end
 
-function vary_epsilon2_epsilon3(beta2, beta3, epsilon2, epsilon3)
-    m = length(epsilon2)
-    n = length(epsilon3)
+function vary_rho_epsilon2(k, q, rho, beta2, beta3, epsilon2, epsilon3)
+    m = length(rho)
+    n = length(epsilon2)
 
-    psi = zeros(m, n)
-    nu = zeros(m, n)
+    psi1 = zeros(m, n)
+    nu1 = zeros(m, n)
+    psi2 = zeros(m, n)
+    nu2 = zeros(m, n)
 
-    for i in eachindex(epsilon2)
-        e2 = epsilon2[i]
-        for j in eachindex(epsilon3)
-            e3 = epsilon3[j]
-            psi[i, j], nu[i, j] = get_polarization(k, q, rho, epsilon2, epsilon3, beta2, beta3)
+    for i in eachindex(rho)
+        r = rho[i]
+        for j in eachindex(epsilon2)
+            e2 = epsilon2[j]
+            psi1[i, j], nu1[i, j], psi2[i, j], nu2[i, j] = get_polarization(k, q, r, e2, epsilon3, beta2, beta3)
 
             println((i, j, psi[i, j]))
             flush(stdout)
         end
     end
-    return psi, nu
+    return psi1, nu1, psi2, nu2
 end
 
-function vary_epsilon2_epsilon3_distributed(beta2, beta3, epsilon2, epsilon3)
-    m = length(epsilon2)
-    n = length(epsilon3)
+function vary_rho_epsilon2_distributed(k, q, rho, beta2, beta3, epsilon2, epsilon3)
+    m = length(rho)
+    n = length(epsilon2)
 
-    psi = zeros(m, n)
-    nu = zeros(m, n)
+    psi1 = zeros(m, n)
+    nu1 = zeros(m, n)
+    psi2 = zeros(m, n)
+    nu2 = zeros(m, n)
     
-    for i in eachindex(epsilon2)
-        e2 = epsilon2[i]
-        results = pmap(e3 -> get_polarization(k, q, rho, e2, e3, beta2, beta3), epsilon3)
-        psi[i, :], nu[i, :] = unzip(results)
+    for i in eachindex(rho)
+        r = rho[i]
+        results = pmap(e2 -> get_polarization(k, q, r, e2, epsilon3, beta2, beta3), epsilon2)
+        psi1[i, :], nu1[i, :], psi2[i, :], nu2[i, :] = unzip(results)
         println(i)
         flush(stdout)
     end
     
-    return psi, nu
+    return psi1, nu1, psi2, nu2
 end
 
 
 m = 100
 n = 100
 
+k = 20
+q = 20
+rho = LinRange(0, 1.0, m)
 epsilon2 = LinRange(0, 1.0, m)
-epsilon3 = LinRange(0.8, 1.0, n)
-
-e2 = 0.5
-e3 = 0.95
-
-beta2 = LinRange(0, 1.0, m)
-beta3 = LinRange(1.0, 7.0, n)
-
-b2 = 0.2
-b3 = 4
+epsilon3 = 0.95
+beta2 = 0.2/k
+beta3 = 4/q
 
 if in_parallel
-    psi1, nu1 = vary_epsilon2_epsilon3_distributed(b2, b3, epsilon2, epsilon3)
-    psi2, nu2 = vary_beta2_beta3_distributed(beta2, beta3, e2, e3)
+    psi1, nu1, psi2, nu2 = vary_rho_epsilon2_distributed(k, q, rho, beta2, beta3, epsilon2, epsilon3)
 else
-    psi1, nu1 = vary_epsilon2_epsilon3(b2, b3, epsilon2, epsilon3)
-    psi2, nu2 = vary_beta2_beta3(beta2, beta3, e2, e3)
+    psi1, nu1, psi2, nu2 = vary_rho_epsilon2(k, q, rho, beta2, beta3, epsilon2, epsilon3)
 end
 
-data1 = Dict("beta2"=>b2, "beta3"=>b3, "epsilon2"=>epsilon2, "epsilon3"=>epsilon3, "psi"=>psi1, "nu"=>nu1)
-data2 = Dict("beta2"=>beta2, "beta3"=>beta3, "epsilon2"=>e2, "epsilon3"=>e3, "psi"=>psi2, "nu"=>nu2)
+data = Dict("k"=>k, "q"=>q, "epsilon2"=>epsilon2, "epsilon3"=>epsilon3, "rho"=>rho, "beta2"=>beta2, "beta3"=>beta3, "psi1"=>psi1, "nu1"=>nu1, "psi2"=>psi2, "nu2"=>nu2)
 
-open("Data/stability/theoretical_rho_epsilon3_polarization.json","w") do f
-  JSON.print(f, data1)
-end
-
-open("Data/stability/theoretical_beta2_beta3_polarization.json","w") do f
-    JSON.print(f, data2)
+open("Data/stability/theoretical_rho_epsilon2_polarization.json","w") do f
+  JSON.print(f, data)
 end
