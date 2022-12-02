4e6c8ca5eaffadbd5ecd67ccd86f027bb70ae189
Nicholas Landry <nicholas.landry.91@gmail.com>
2022-12-02 16:33:11 -0500

    reorganization and fixes

---
+++ Extra/SIS_vs_epsilon2_epsilon3_max.py
@@ -0,0 +1,81 @@
+import json
+import os
+from fileinput import filename
+
+import numpy as np
+import xgi
+
+import HypergraphContagion
+
+# Epidemic parameters
+gamma = 1
+tmax = 100
+fraction_to_average = 0.1
+is_verbose = True
+num_processes = len(os.sched_getaffinity(0))
+
+num_sims = 1
+
+output_filename = "polarization.json"
+
+with open("Data/SBM/hypergraphs/epsilon_values.json") as file:
+    data = json.loads(file.read())
+epsilon2 = data["epsilon2"]
+epsilon3 = data["epsilon3"]
+n = len(epsilon2)
+m = len(epsilon3)
+
+data = dict()
+
+for e2 in epsilon2:
+    for e3 in epsilon3:
+        fname = f"Data/SBM/hypergraphs/{e2}-{e3}.json"
+        H = xgi.read_hypergraph_json(fname)
+        community1 = set(list(H.nodes)[: int(H.num_nodes / 2)])
+        community2 = set(list(H.nodes)[int(H.num_nodes / 2) :])
+        mean_link_degree = H.nodes.degree(order=1).mean()
+        mean_triangle_degree = H.nodes.degree(order=2).mean()
+
+        beta2c = gamma / mean_link_degree
+        beta3c = gamma / mean_triangle_degree
+
+        beta2min = 0.0 * beta2c
+        beta2max = 1 * beta2c
+
+        beta3min = 2 * beta3c
+        beta3max = 10 * beta3c
+
+        beta2 = np.linspace(beta2min, beta2max, n)
+        beta3 = np.linspace(beta3min, beta3max, m)
+
+        arglist = list()
+        for b2 in beta2:
+            for b3 in beta3:
+                beta = {2: b2, 3: b3}
+                arglist.append(
+                    (
+                        fname,
+                        gamma,
+                        beta,
+                        community1,
+                        community2,
+                        tmax,
+                        fraction_to_average,
+                        num_sims,
+                        is_verbose,
+                    )
+                )
+        print(f"epsilon2={e2}, epsilon3={e3} started", flush=True)
+        psi = HypergraphContagion.get_polarization_in_parallel(arglist, num_processes)
+        psi = np.reshape(psi, [n, m], order="C")
+        print(f"epsilon2={e2}, epsilon3={e3} completed", flush=True)
+
+        data[f"gamma-{e2}-{e3}"] = gamma
+        data[f"beta2-{e2}-{e3}"] = beta2
+        data[f"beta3-{e2}-{e3}"] = beta3
+        data[f"psi-{e2}-{e3}"] = psi
+
+datastring = json.dumps(data)
+
+with open(f"Data/stability/{output_filename}", "w") as output_file:
+    output_file.write(datastring)
---
+++ mean-field_polarization_boundaries.jl
@@ -0,0 +1,142 @@
+# Gets the stability of the rescaled homogeneous equations
+using Distributed, JSON
+
+in_parallel = true
+
+if in_parallel
+    Distributed.addprocs(Sys.CPU_THREADS; exeflags="--project")
+    @everywhere using Distributed, IntervalArithmetic, IntervalRootFinding, StaticArrays, LinearAlgebra
+else
+    using IntervalArithmetic, IntervalRootFinding, StaticArrays, LinearAlgebra
+end
+
+
+@everywhere begin
+unzip(a) = map(x->getfield.(a, x), fieldnames(eltype(a)))
+
+function get_polarization(epsilon2, epsilon3, beta2, beta3)
+    psi_max = 0
+    nu_max = Inf
+
+    function f((x1, x2))
+        dxdt1 = -x1 + 0.5*beta2*(1 - x1)*(x1 + x2 + epsilon2*(x1 - x2)) + 0.25*beta3*(1 - x1)*((x1 + x2)^2 + epsilon3*(3*x1^2 - 2*x1*x2 - x2^2))
+        dxdt2 = -x2 + 0.5*beta2*(1 - x2)*(x2 + x1 + epsilon2*(x2 - x1)) + 0.25*beta3*(1 - x2)*((x2 + x1)^2 + epsilon3*(3*x2^2 - 2*x2*x1 - x1^2))
+        return SVector(dxdt1, dxdt2)
+    end
+
+    function jacobian((x1, x2))
+        J = zeros(2, 2)
+        J[1, 1] = -1 + 0.5*beta2*(1 - x1)*(1 + epsilon2) - 0.5*beta2*(x1 + x2 + epsilon2*(x1 - x2)) +
+                0.5*beta3*(1 - x1)*(x1 + x2 + epsilon3*(3*x1 - x2)) - 0.25*beta3*((x1 + x2)^2 + epsilon3*(3*x1^2 - 2*x1*x2 - x2^2))
+        J[1, 2] = 0.5*beta2*(1 - x1)*(1 - epsilon2) + 0.5*beta3*(1 - x1)*(x1 + x2 - epsilon3*(x1 + x2))
+        J[2, 1] = 0.5*beta2*(1 - x2)*(1 - epsilon2) + 0.5*beta3*(1 - x2)*(x2 + x1 - epsilon3*(x2 + x1))
+        J[2, 2] = -1 + 0.5*beta2*(1 - x2)*(1 + epsilon2) - 0.5*beta2*(x2 + x1 + epsilon2*(x2 - x1)) +
+                0.5*beta3*(1 - x2)*(x2 + x1 + epsilon3*(3*x2 - x1)) - 0.25*beta3*((x2 + x1)^2 + epsilon3*(3*x2^2 - 2*x2*x1 - x1^2))
+        return J
+    end
+
+    function spectral_abscissa(J)
+        return maximum(real(eigvals(J)))
+    end
+    
+    result = roots(f, IntervalBox(0.0..0.5, 0.5..1.0))
+    for rt in result
+        x = mid(rt.interval)
+        psi = abs(x[1] - x[2])
+
+
+        if psi > 0.01 # filter out symmetric roots to save time
+            nu = spectral_abscissa(jacobian(x))
+            
+            if (psi > psi_max && nu < 0)
+                psi_max = psi
+                nu_max = nu
+            end
+        end
+    end
+
+    return psi_max, nu_max
+end
+
+end
+
+
+function vary_beta2_beta3(beta2, beta3, epsilon2, epsilon3)
+    m = length(beta2)
+    n = length(beta3)
+
+    psi = zeros(m, n)
+    nu = zeros(m, n)
+
+    for i in eachindex(beta2)
+        b2 = beta2[i]
+        for j in eachindex(beta3)
+            b3 = beta3[j]
+            psi[i, j], nu[i, j] = get_polarization(epsilon2, epsilon3, b2, b3)
+
+            println((i, j, psi[i, j]))
+            flush(stdout)
+        end
+    end
+    return psi, nu
+end
+
+function vary_beta2_beta3_distributed(beta2, beta3, epsilon2, epsilon3)
+    m = length(beta2)
+    n = length(beta3)
+
+    psi = zeros(m, n)
+    nu = zeros(m, n)
+    
+    for i in eachindex(beta2)
+        b2 = beta2[i]
+        results = pmap(b3 -> get_polarization(epsilon2, epsilon3, b2, b3), beta3)
+        psi[i, :], nu[i, :] = unzip(results)
+        println(i)
+        flush(stdout)
+    end
+    
+    return psi, nu
+end
+
+
+k = 5
+l = 5
+
+epsilon2 = LinRange(0.0, 1.0, m)
+epsilon3 = LinRange(0.95, 1.0, n)
+
+m = 101
+n = 101
+
+beta2 = LinRange(0, 0.5, m)
+beta3 = LinRange(3.0, 6.0, n)
+
+
+data1 = Dict("beta2"=>beta2, "beta3"=>beta3, "epsilon2"=>epsilon2, "epsilon3"=>1.0)
+data2 = Dict("beta2"=>beta2, "beta3"=>beta3, "epsilon2"=>1.0, "epsilon3"=>epsilon3)
+
+if in_parallel
+    for e2 in epsilon2
+        psi1, nu1 = vary_beta2_beta3_distributed(beta2, beta3, e2, 1.0)
+    end
+
+    for e3 in epsilon3
+        psi2, nu2 = vary_beta2_beta3_distributed(beta2, beta3, 1.0, e3)
+    end
+
+else
+    psi1, nu1 = vary_epsilon2_epsilon3(b2, b3, epsilon2, epsilon3)
+    psi2, nu2 = vary_beta2_beta3(beta2, beta3, e2, e3)
+end
+
+data1 = Dict("beta2"=>b2, "beta3"=>b3, "epsilon2"=>epsilon2, "epsilon3"=>epsilon3, "psi"=>psi1, "nu"=>nu1)
+data2 = Dict("beta2"=>beta2, "beta3"=>beta3, "epsilon2"=>e2, "epsilon3"=>e3, "psi"=>psi2, "nu"=>nu2)
+
+open("Data/stability/mean-field_epsilon2_epsilon3_polarization.json","w") do f
+  JSON.print(f, data1)
+end
+
+open("Data/stability/mean-field_beta2_beta3_polarization.json","w") do f
+    JSON.print(f, data2)
+end
